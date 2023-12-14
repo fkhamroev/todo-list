4c2ba7fd2067a7cf262eb22570ab3034b56a8f3c
Firuz Khamroev <fkhamroev@icloud.com>
2023-12-14 18:31:15 +0500

    second

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/fkhamroev/todo-list/",
+  base: "https://fkhamroev.github.io/todo-list",
   plugins: [react()],
 });
